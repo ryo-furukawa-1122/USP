2bbeb27f3a10de415cdaec51e0f88d36e719c926
Ryo Furukawa <furu.4se@gmail.com>
2024-05-03 15:07:43 +0900

    Apply calculation function

--- usp/main.py
+++ usp/main.py
@@ -1,11 +1,14 @@
 import flet as ft
-
+import numpy as np
+from scipy import integrate
+import matplotlib.pyplot as plt
 
 def main(page: ft.Page):
     page.title = "USP"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     # page.horizontal_alignment = ft.MainAxisAlignment.CENTER
     # page.adaptive = True
+    page.scroll = True
     page.appbar = ft.AppBar(
         title=ft.Text("Ultrasound Parameters"),
         center_title=True,
@@ -13,65 +16,103 @@ def main(page: ft.Page):
     )
 
     def calculate(e):
-        value = 0.0
-        i1.value = str(value + 0)
-        i2.value = str(value + 0)
+        tbd = (float(dc.value) / 100) / (float(prf.value) * 1e3)
+        z = float(rho.value) * float(c.value)
+        a = float(amplitude.value) * 1e6
+        f = float(frequency.value) * 1e6
+        t = np.arange(0, tbd, tbd/1e6)
+        p = a * np.sin(2 * np.pi * f * t)
+        pii = integrate.cumtrapz((p ** 2) / z, t, initial=0)
+        if dd.value == "Pulse Wave Mode":
+            value1 = pii[-1] / t[-1]  # in W/m^2
+            value1 *= 1e-4  # in W/cm^2
+            value2 = value1 * (float(dc.value) / 100)
+        else:
+            value1 = pii[-1] / tbd  # in W/m^2
+            value1 *= 1e-4  # in W/cm^2
+            value2 = value1
+        i1text.value = "{:.2f}".format(value1)
+        i2text.value = "{:.2f}".format(value2)
         page.update()
     
-    btn = ft.ElevatedButton(text="Calculate", on_click=calculate, width=300)
-    i1 = ft.TextField(value="0", text_align=ft.TextAlign.LEFT, width=100)
-    i2 = ft.TextField(value="0", text_align=ft.TextAlign.LEFT, width=100)
+    btn = ft.ElevatedButton(text="Calculate", on_click=calculate, width=400, height=50)
+
+    # Input parameters
+    dd = ft.Dropdown(
+                width=200,
+                options=[
+                    ft.dropdown.Option("Pulse Wave Mode"),
+                    ft.dropdown.Option("Continuous Wave Mode"),
+                ],
+            )
+    amplitude = ft.TextField(value="0.1", text_align=ft.TextAlign.LEFT, width=100)
+    frequency = ft.TextField(value="0.5", text_align=ft.TextAlign.LEFT, width=100)
+    dc = ft.TextField(value="50", text_align=ft.TextAlign.LEFT, width=100)
+    prf = ft.TextField(value="1", text_align=ft.TextAlign.LEFT, width=100)
+    rho = ft.TextField(value="1000", text_align=ft.TextAlign.LEFT, width=100)
+    c = ft.TextField(value="1480", text_align=ft.TextAlign.LEFT, width=100)
+
+    # Output parameters
+    i1text = ft.Text(value="", text_align=ft.TextAlign.LEFT)
+    i2text = ft.Text(value="", text_align=ft.TextAlign.LEFT)
 
     page.add(
         ft.Row(
             [
                 ft.Column(
                     [
-                        # ft.Checkbox(value=False, label="Continuous Wave Mode"),
                         ft.Row(
                             [
                                 ft.Text("Wave Mode", width=100),
-                                ft.Dropdown(
-                                    width=200,
-                                    options=[
-                                        ft.dropdown.Option("Pulse Wave Mode"),
-                                        ft.dropdown.Option("Continuous Wave Mode"),
-                                    ],
-                                ),
+                                dd,
                             ]
                         ),
                         ft.Row(
                             [
                                 ft.Text("Amplitude (MPa):"),
-                                ft.TextField(value="0", text_align=ft.TextAlign.LEFT, width=100),
+                                amplitude,
                             ]
                         ),
                         ft.Row(
                             [
                                 ft.Text("Center frequency (MHz):"),
-                                ft.TextField(value="0", text_align=ft.TextAlign.LEFT, width=100),
+                                frequency,
                             ]
                         ),
                         ft.Row(
                             [
                                 ft.Text("Duty cycle (%):"),
-                                ft.TextField(value="0", text_align=ft.TextAlign.LEFT, width=100),
+                                dc,
                             ]
                         ),
                         ft.Row(
                             [
                                 ft.Text("PRF (kHz):"),
-                                ft.TextField(value="0", text_align=ft.TextAlign.LEFT, width=100),
+                                prf,
+                            ]
+                        ),
+                        ft.Row(
+                            [
+                                ft.Text("Density (kg/m^3):"),
+                                rho,
+                            ]
+                        ),
+                        ft.Row(
+                            [
+                                ft.Text("Speed of sound (m/s):"),
+                                c,
                             ]
                         ),
                         btn,
                         ft.Row([
-                            ft.Text("I_sppa (W/cm^2):"),
-                            i1
+                            ft.Text("Spatial-peak pulse-average intensity: "),
+                            i1text,
+                            ft.Text(f" (W/cm^2)"),
                         ]),
                         ft.Row([
-                            ft.Text("I_spta (W/cm^2):"),
-                            i2
+                            ft.Text("Spatial-peak temporal-average intensity: "),
+                            i2text,
+                            ft.Text(f" (W/cm^2)"),
                         ])
                     ],
                 ),
